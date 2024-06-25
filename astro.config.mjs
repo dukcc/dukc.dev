ce76454c546b7c42ed99522aa78d50538027d89d
dukc <dukkcc@proton.me>
2024-06-25 08:02:42 -0700

    final touches

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import swup from "@swup/astro";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
